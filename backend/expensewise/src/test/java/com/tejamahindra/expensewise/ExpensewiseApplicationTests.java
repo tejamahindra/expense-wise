9d0bd50f836d323eee999f66e6a843f75913a49f
tejamahindra <tejamahindramalleswarapu@gmail.com>
2026-03-28 17:56:01 +0530

    feat: Spring Boot backend setup with JWT dependencies and folder structure

---
+++ backend/expensewise/src/test/java/com/tejamahindra/expensewise/ExpensewiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tejamahindra.expensewise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensewiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
